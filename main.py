aa07ee39534846606270e171e40d45215a245a4e
Josue <josuedac1888@gmail.com>
2022-01-10 01:16:24 -0600

    NuevaVersion

--- main.py
+++ main.py
@@ -4,25 +4,8 @@ import xlsxwriter
 import json
 import openpyxl
 
-def maxColum(sheet):
-    auxtemp = 0
-    columna = 0
-    if sheet['B' + str(5)].value is None:
-        columna = 2
-    else:
-        for j in range(26):
-            auxtemp = 0
-            for m in range(1, sheet.max_row-4):
-                if sheet[chr(j + 97) + str(m+4)].value is None:
-                    auxtemp = auxtemp + 1
-            print(auxtemp)
-            if auxtemp == sheet.max_row-5:
-                columna = j
-                break
-    return columna+1
-
 #idTorneo es el identificador del torneo el cual debe de ser anexado a la variable url;
-idTorneo = "XqClhOsm"
+idTorneo = "xKLELqoU"
 url = 'https://lichess.org/api/tournament/'+idTorneo+'/results'
 r = requests.get(url, allow_redirects=True)
 #Creamos los documentos con la información requerida para crear nuestra lista de rendimiento.
@@ -55,24 +38,48 @@ torneoActual = openpyxl.load_workbook('C:/creaListaDerendimiento/preview.xlsx')
 rendimientoActual = openpyxl.load_workbook('C:/creaListaDerendimiento/final.xlsx')
 sheet_rangesTA = torneoActual['Sheet1']
 sheet_rangesRA = rendimientoActual['Sheet1']
-print("Las filas maximas del documento es:"+str(sheet_rangesRA.max_row))
-print("El numero maximo de columas es:"+str(maxColum(sheet_rangesRA))+"En otras palabras en la letra:"+chr(maxColum(sheet_rangesRA)+96))
-columnaMax = maxColum(sheet_rangesRA)
-aux = 0
-for x in range(1, sheet_rangesTA.max_row + 1):
-    if sheet_rangesRA['A' + str(x + 4)].value is None:
-        sheet_rangesRA['A' + str(x + 4)].value = sheet_rangesTA['A' + str(x)].value
-        sheet_rangesRA[chr(columnaMax+96) + str(x + 4)].value = sheet_rangesTA['B' + str(x)].value
-    else:
-        for y in range(1, sheet_rangesTA.max_row + 1):
-            if sheet_rangesRA['A'+str(x+4)].value == sheet_rangesTA['A' + str(y)].value:
-                print(sheet_rangesRA['A'+str(x+4)].value)
-                sheet_rangesRA[chr(columnaMax + 96) + str(x + 4)].value = sheet_rangesTA['B' + str(x)].value
-            else:
-                aux =+ 1
-            if(aux == sheet_rangesTA.max_row + 1):
-                sheet_rangesRA['A'+str(sheet_rangesTA.max_row + 1)].value = sheet_rangesTA['A' + str(y)].value
-                sheet_rangesRA[chr(columnaMax + 96) + str(x + 4)].value = sheet_rangesTA['B' + str(x)].value
+#La funcion .max_row encuentra exactamente el numero de filas, si excel tiene n con esta funcion devolveremos n
+#print("Las filas maximas del documento es:"+str(sheet_rangesRA.max_row))
+#El numero de columas del documento lo obtendremos con cuantos torneos han sido contabilizados ya
+columnaMax = sheet_rangesRA['B4'].value + 1
+caracterColumna = ""+chr(columnaMax + 97)
+columnaTotal = chr(columnaMax + 97 + 1)
+#Limpiamos residuos del total
+if columnaMax !=1:
+    for a in range(sheet_rangesRA.max_row - 5):
+        sheet_rangesRA[caracterColumna + str(a + 5)] = ""
+#print("El numero maximo de columas es:"+ str(columnaMax)+" En otras palabras en la letra:"+caracterColumna)
+#Al agregar un nuevo torneo este debe de ser contabilizado
+sheet_rangesRA['B4'].value = sheet_rangesRA['B4'].value + 1
+#Comenzamos a traspasar la información del nuevo torneo
+if columnaMax == 1:
+    for x in range(sheet_rangesTA.max_row):
+        sheet_rangesRA['A' + str(x + 6)].value = sheet_rangesTA['A' + str(x+1)].value
+        sheet_rangesRA["{}{}".format(caracterColumna, str(x + 6))].value = int(sheet_rangesTA["{}{}".format('B', str(x + 1))].value)
+else:
+    for n in range(sheet_rangesRA.max_row-5):
+        for m in range(sheet_rangesTA.max_row):
+            if sheet_rangesRA['A' + str(n + 6)].value == sheet_rangesTA['A' + str(m + 1)].value:
+                #print("Coincidencia encontrada:"+sheet_rangesRA['A' + str(n + 6)].value+"-"+sheet_rangesTA['A' + str(m + 1)].value)
+                #Para que se guarden los numeros como numeros
+                sheet_rangesRA["{}{}".format(caracterColumna, str(n+6))].value = int(sheet_rangesTA["{}{}".format('B', str(m+1))].value)
+                break
+            #print(str(n) + "-" + str(m))
+            #if m == sheet_rangesTA.max_row-1:
+                #print("n no existe en m")
+    for m in range(sheet_rangesTA.max_row):
+        auxRep = 0
+        for n in range(sheet_rangesRA.max_row - 5):
+            if sheet_rangesRA['A' + str(n + 6)].value != sheet_rangesTA['A' + str(m + 1)].value:
+                auxRep = auxRep + 1
+        if auxRep >= sheet_rangesRA.max_row - 5:
+            #print("n no contiene a m:" + sheet_rangesTA['A'+str(m+1)].value)
+            sheet_rangesRA['A' + str(sheet_rangesRA.max_row+1)].value = sheet_rangesTA['A' + str(m + 1)].value
+            sheet_rangesRA["{}{}".format(caracterColumna, str(sheet_rangesRA.max_row))].value = int(sheet_rangesTA["{}{}".format('B', str(m + 1))].value)
+print(sheet_rangesRA.max_row - 5)
+for a in range(sheet_rangesRA.max_row - 5):
+    sheet_rangesRA[columnaTotal + str(a+6)] = "=SUM(B"+str(a+6)+":"+caracterColumna+str(a+6)+")"
+sheet_rangesRA[columnaTotal + str(5)].value = "Total"
 rendimientoActual.save("C:/creaListaDerendimiento/final.xlsx")
 
 
